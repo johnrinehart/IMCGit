1cb614658b212588ebd3b05318fe1ad042f2a5e7
41222611 <41222611@087934-P33S103.macklabs.br>
2015-03-30 11:42:44 -0300

    primeiro commit

---
+++ src/imcgit/IMCGit.java
@@ -0,0 +1,20 @@
+package imcgit;
+
+
+    import javax.swing.JFrame;
+    
+    //Main - Nome da classe
+    public class IMCGit
+    {
+       //Comando principal para execução do programa
+       public static void main(String[] args)
+       {
+          //Criação de um objeto ex da classe Ex2
+          Ex2 ex = new Ex2();
+          //posição da tela ao abri-la
+          ex.setLocation(400,200);
+          //tornando a tela visível
+          ex.setVisible(true);
+       }
+    }
+
